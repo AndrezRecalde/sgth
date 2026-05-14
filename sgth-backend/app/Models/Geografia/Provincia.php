0868d3c0e77cce3a78468f9625ff39941d3fcf68
AndrezRecalde <azw1021@gmail.com>
2026-05-14 14:08:22 -0500

    feat(geo): tablas provincias y ciudades Ecuador

---
+++ sgth-backend/app/Models/Geografia/Provincia.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Geografia;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Provincia extends Model
+{
+    use HasFactory;
+
+    protected $table = 'provincias';
+
+    protected $fillable = [
+        'nombre',
+        'codigo',
+    ];
+
+    public function ciudades(): HasMany
+    {
+        return $this->hasMany(Ciudad::class);
+    }
+}
