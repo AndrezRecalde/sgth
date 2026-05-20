6e061169562cf3916d0a675490799572a65eae57
AndrezRecalde <azw1021@gmail.com>
2026-05-20 12:25:52 -0500

    fix(shell): eliminar any en tablerIcons, usar tipos Tabler correctos

--- sgth-frontend/src/lib/tablerIcons.ts
+++ sgth-frontend/src/lib/tablerIcons.ts
@@ -1,10 +1,16 @@
-import * as Icons from '@tabler/icons-react'
+import * as TablerIcons from '@tabler/icons-react'
+import type { Icon } from '@tabler/icons-react'
+import { IconPoint } from '@tabler/icons-react'
 import React from 'react'
 
-export function getNavIcon(name: string) {
-  const IconComponent = (Icons as any)[name]
+type IconName = keyof typeof TablerIcons
+
+const ICON_SIZE = 18
+
+export function getNavIcon(name: string): React.ReactElement {
+  const IconComponent = TablerIcons[name as IconName] as Icon | undefined
   if (!IconComponent) {
-    return React.createElement(Icons.IconPoint, { size: 18 })
+    return React.createElement(IconPoint, { size: ICON_SIZE })
   }
-  return React.createElement(IconComponent, { size: 18 })
+  return React.createElement(IconComponent, { size: ICON_SIZE })
 }
